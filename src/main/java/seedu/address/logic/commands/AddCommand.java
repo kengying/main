ae33f4361cd03bc5483c618b964df960e98729e6
kengying <L.kengying@gmail.com>
2017-11-12 10:48:27 +0800

    Add whitespace

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -26,7 +26,7 @@ public class AddCommand extends UndoableCommand {
             + "Parameters: "
             + PREFIX_NAME + "NAME "
             + PREFIX_GROUP + "GROUP "
-            + "[" +PREFIX_PHONE + "PHONE] "
+            + "[" + PREFIX_PHONE + "PHONE] "
             + "[" + PREFIX_EMAIL + "EMAIL] "
             + "[" + PREFIX_ADDRESS + "ADDRESS] "
             + "[" + PREFIX_TAG + "TAG]...\n"
